ef4c9a711a8429c2c6416e654dbc628683c05eab
Taylor Everts <tayloreverts@quickenloans.com>
2018-10-09 10:58:26 -0400

    First Lab project

---
+++ Lab2GrandRiver/Lab2GrandRiver/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2GrandRiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Length = 0;
+            double Width = 0;
+            string Continue = "y";
+
+            while (Continue == "y")
+            {
+                Console.WriteLine("Welcome to Grand Circus' Room Detail Generator!");
+                Console.WriteLine("Enter Length: ");
+                Length = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Width: ");
+                Width = double.Parse(Console.ReadLine());
+                Console.WriteLine("Area: " + (Length * Width));
+                Console.WriteLine("Perimeter: " + (Length * 2 + Width * 2));
+                Console.WriteLine("Continue? (y/n)");
+                Continue = Console.ReadLine();
+                Continue.ToLower();
+            }
+
+        }
+    }
+}
